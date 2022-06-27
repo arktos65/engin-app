3d0623a98d1d9e668084c6febb39d11f3fc23b6e
sean.sullivan <sean.sullivan@beatport.com>
2022-06-27 09:17:30 -0600

    Refactored Department model spec to simplify tests.

--- spec/models/department_spec.rb
+++ spec/models/department_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Department, type: :model do
+  subject {
+    described_class.new(name: "Engineering",
+                        division: "Technology",
+                        manager: "Mary Smith")
+  }
   it "is valid with valid attributes" do
-    department = Department.new
-    department.name = "Engineering"
-    department.division = "Technology"
-    department.manager = "Mary Smith"
-    expect(department).to be_valid
+    expect(subject).to be_valid
   end
   it "is not valid without a department name" do
-    department = Department.new(name: nil)
-    expect(department).to_not be_valid
+    subject.name = nil
+    expect(subject).to_not be_valid
   end
 end
