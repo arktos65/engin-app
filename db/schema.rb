cdc0fe66cb7638447104e6d1b12d09ae0e16c988
sean.sullivan <sean.sullivan@beatport.com>
2022-06-27 14:35:02 -0600

    Applied limits to string storage length in the models.

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_27_160117) do
+ActiveRecord::Schema.define(version: 2022_06_27_202948) do
+
+  create_table "currencies", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "symbol", limit: 10, null: false
+    t.decimal "USDExchange", precision: 10, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["symbol"], name: "index_currencies_on_symbol"
+  end
 
   create_table "departments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "division"
-    t.string "manager"
+    t.string "name", limit: 50, null: false
+    t.string "division", limit: 50
+    t.string "manager", limit: 50
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["name"], name: "index_departments_on_name", unique: true
   end
 
   create_table "roles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name", limit: 50, null: false
     t.boolean "shared", default: false
     t.boolean "administration", default: false
     t.datetime "created_at", precision: 6, null: false
@@ -31,20 +39,20 @@ ActiveRecord::Schema.define(version: 2022_06_27_160117) do
   end
 
   create_table "sources", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "contact", null: false
-    t.string "contact_title"
-    t.string "contact_phone"
-    t.string "contact_email", null: false
+    t.string "name", limit: 50, null: false
+    t.string "contact", limit: 50, null: false
+    t.string "contact_title", limit: 50
+    t.string "contact_phone", limit: 50
+    t.string "contact_email", limit: 80, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["name"], name: "index_sources_on_name"
   end
 
   create_table "value_streams", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "stakeholder"
-    t.string "stakeholder_email"
+    t.string "name", limit: 50, null: false
+    t.string "stakeholder", limit: 50
+    t.string "stakeholder_email", limit: 50
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["name"], name: "index_value_streams_on_name"
