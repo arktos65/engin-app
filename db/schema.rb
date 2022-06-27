f21ecfa50b4c829e0b28f3524e903ce9b8365e87
sean.sullivan <sean.sullivan@beatport.com>
2022-06-27 09:54:35 -0600

    Added Value Stream model, spec, and Trestle resource.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_27_152745) do
+ActiveRecord::Schema.define(version: 2022_06_27_154744) do
 
   create_table "departments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 2022_06_27_152745) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["name"], name: "index_sources_on_name"
+  end
+
+  create_table "value_streams", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "stakeholder"
+    t.string "stakeholder_email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_value_streams_on_name"
   end
 
 end
