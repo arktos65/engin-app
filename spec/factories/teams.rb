3d9673ec7a12e769d82238c994bc2878584b2bb5
sean.sullivan <sean.sullivan@beatport.com>
2022-07-07 07:59:36 -0600

    Refactored factories to use correct syntax for Faker::Number method.

--- spec/factories/teams.rb
+++ spec/factories/teams.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :team do
     member_id { nil }
     project_id { nil }
-    annual_rate { Faker::Number.between(80000..130000) }
+    annual_rate { Faker::Number.between(from: 80000, to: 130000) }
     weekly_rate { nil }
     currency_id { nil }
     start_date { Faker::Date.backward(days: 45) }
