6650740c556281ba4d7100618fad6edeb6e2af3c
sean.sullivan <sean.sullivan@beatport.com>
2022-07-11 13:48:15 -0600

    Refactored ProjectConnector module.

--- app/controllers/jira_issues_controller.rb
+++ app/controllers/jira_issues_controller.rb
@@ -1,11 +1,13 @@
 class JiraIssuesController < ApplicationController
+  include ProjectConnector
+
   def index
-    client = JIRA::Client.new(JiraConnect.get_connector)
+    client = JIRA::Client.new(get_options)
     issues = client.Issue.all
   end
 
   def show
-    client = JIRA::Client.new(JiraConnect.get_connector)
+    client = JIRA::Client.new(get_options)
     isue = client.Issue.find(params[:id])
   end
 end
