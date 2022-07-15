67c8c367503895444b2c0fdccd70ac2d5537682a
sean.sullivan <sean.sullivan@beatport.com>
2022-07-15 10:09:47 -0600

    Added a heartbeat job for monitoring server health.

---
+++ app/jobs/heartbeat_job.rb
@@ -0,0 +1,14 @@
+# app/jobs/heartbeat_job.rb
+#
+# This job runs in the background as a heartbeat and can be used to monitor
+# if Sidekiq is running correctly.  It runs every 15 seconds and logs an alive
+# message with a timestamp.
+#
+class HeartbeatJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    sleep 15
+    p "EngIn App alive #{Time.now().strftime('%F - %H:%M:%S.%L')}"
+  end
+end
